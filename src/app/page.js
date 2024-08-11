2ba87ed4f05f6cce9152ce4a4d548548e826f9d1
Leandrodeleon22 <leandrodeleon22@gmail.com>
2024-08-11 17:32:52 -0400

    input field in main route

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,13 @@
 export default function Home() {
   return (
     <main className="bg-[url('/main-bg.png')] min-h-screen bg-no-repeat bg-cover">
-      main
+      <div className="flex justify-center items-center min-h-screen ">
+        <input
+          type="password"
+          placeholder="Enter the password"
+          className="py-2 px-6 w-full max-w-[48.9rem] rounded-lg text-[2rem] text-center"
+        />
+      </div>
     </main>
   );
 }
