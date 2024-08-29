40d114bd1e18aaccdcef44175febe8b924d55902
Leandrodeleon22 <leandrodeleon22@gmail.com>
2024-08-29 09:03:45 -0400

    main page style fix

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="bg-[url('https://res.cloudinary.com/da8jnpdza/image/upload/v1723424535/main-bg_mx6ykj.png')] min-h-screen bg-no-repeat bg-cover">
       <form
-        className="flex justify-center items-center min-h-screen flex-col"
+        className="flex justify-center items-center min-h-screen flex-col mx-14"
         action={inputPassword}
       >
         <input
@@ -12,7 +12,7 @@ export default function Home() {
           type="password"
           placeholder="Enter the password"
           name="password"
-          className="py-2 px-6 w-full max-w-[48.9rem] rounded-lg text-[2rem] text-center"
+          className="py-2  px-6 w-full max-w-[48.9rem] rounded-lg text-[2rem] text-center"
         />
         <button
           type="submit"
